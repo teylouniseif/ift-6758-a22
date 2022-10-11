026dbe59dfefbee80b70353f5ebe206e864b6d14
raphaeltremblay <80924999+raphaeltremblay@users.noreply.github.com>
2022-10-11 15:09:25 -0400

    Add files via upload
    New version of question 4 which now allows to iterate through every file of a directory and create a meta dF

--- question4.py
+++ question4.py
@@ -1,12 +1,30 @@
 import pandas as pd
 import json
 import tqdm
+import os
+
+#The function create_full_df can now create a DF with all files in a directory
 
 
-#ONLY DOING ONE GAME FOR NOW, BUT SEEMS TO BE WORKING WELL -RAPH
 
+def create_full_df(directory: str)->pd.DataFrame:
+    """
+    Function that takes a directory of json game files, iterates through all of them to create a pd.Dataframe
+    where each event of every match of every season in te directory represent one row.
+    Only the events of type "Shot" and "Goal" are added to the dataframe
+    """
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    directory = os.path.join(current_dir, directory)
+    dfs = []
+    for fileName in os.listdir(directory):
+        f = os.path.join(directory, fileName)
+        if os.path.isfile(f):
+            dfs.append(get_df_from_game(f))
+        if os.path.isdir(f):
+            dfs.append(create_full_df(f))
+    df_merged = pd.concat(dfs, ignore_index=True)
+    return df_merged
 
-filePath = r"C:\Users\raph_\PycharmProjects\DS-GroupProject\data_saved\play_by_play\2017\regular\2017020017.json"
 def get_df_from_game(filePath: str)->pd.DataFrame:
     """
     Function that takes a parameter filePath that corresponds to a json file with all the events of a hockey game
@@ -15,7 +33,7 @@ def get_df_from_game(filePath: str)->pd.DataFrame:
     gameDF = pd.DataFrame()
     try:
         with open(filePath,'r') as f:
-             data = json.loads(f.read())
+            data = json.loads(f.read())
         gameID = data['gamePk']
         rawDF = pd.json_normalize(data['liveData']['plays']['allPlays'])
         rawDF = rawDF[rawDF['result.event'].isin(["Goal","Shot"])]
@@ -36,13 +54,16 @@ def get_df_from_game(filePath: str)->pd.DataFrame:
 
         for (i, ex) in (rawDF.iterrows()):
             #Making sure the shooter and goalie name are accessed properly and that any "assist" is ignored
-            shooter = ex['players'][0]['player']['fullName']
-            if ex['players'][1]["playerType"]!="Assist":
-                goalie = ex['players'][1]['player']['fullName']
-            elif ex['players'][2]["playerType"]=="Goalie":
-                goalie = ex['players'][2]['player']['fullName']
-            else:
-                goalie = "None"
+            for i in range(len(ex['players'])):
+                if (ex['players'][i]["playerType"] == "Shooter" or ex['players'][i]["playerType"] == "Scorer"):
+                    shooters.append(ex['players'][i]["player"]["fullName"])
+            goalie = ""
+            for i in range(len(ex['players'])):
+                if (ex['players'][i]["playerType"] == "Goalie"):
+                    goalie = ex['players'][i]["player"]["fullName"]
+                    goalies.append(goalie)
+            if goalie == "":
+                goalies.append("None")
             eventID.append(ex["about.eventIdx"])
             periodNum.append(ex["about.period"])
             periodTime.append(ex["about.periodTime"])
@@ -58,23 +79,32 @@ def get_df_from_game(filePath: str)->pd.DataFrame:
             gameTime.append(float(str(time[0])+'.'+str(time[1])))
             shotGoal.append(ex["result.event"])
             shotType.append(ex["result.secondaryType"])
-            shooters.append(shooter)
             teamsShot.append(ex["team.triCode"])
-            goalies.append(goalie)
-            emptyNet.append(ex["result.emptyNet"])
+            try:
+                emptyNet.append(ex["result.emptyNet"])
+            except Exception as e:
+                emptyNet.append("NaN")
             goalStrength.append(ex["result.strength.name"])
             xCoord.append(ex['coordinates.x'])
             yCoord.append(ex['coordinates.y'])
         gameDF = pd.DataFrame(gameIDs, columns= ["Game_ID"])
         gameDF = gameDF.assign(Event_ID=eventID, Period_Number=periodNum, Period_Time=periodTime, Game_Time=gameTime,
-                               Shot_or_Goal=shotGoal, Shot_Type=shotType, Shooter=shooters, Team_of_Shooter=teamsShot,
-                               Goalie=goalies, Empty_Net=emptyNet, Goal_Strength=goalStrength, X_Coordinate=xCoord,
-                               Y_Coordinate=yCoord)
+                                   Shot_or_Goal=shotGoal, Shot_Type=shotType, Shooter=shooters, Team_of_Shooter=teamsShot,
+                                   Goalie=goalies, Empty_Net=emptyNet, Goal_Strength=goalStrength, X_Coordinate=xCoord,
+                                   Y_Coordinate=yCoord)
     except Exception as e:
         print(e)
     return gameDF
 
 if __name__ == "__main__":
-    df = get_df_from_game(filePath)
+        #df = get_df_from_game(r"C:\Users\raph_\PycharmProjects\DS-GroupProject\data_saved\play_by_play\2017\regular\2017020462.json")
+        #with pd.option_context('display.max_rows', None,
+#                               'display.max_columns', None,
+#                               'display.precision', 3,
+#                               ):
+#            print(df)
+    directory = r'data_saved'
+    df = create_full_df(directory=directory)
     print(df)
+
 
