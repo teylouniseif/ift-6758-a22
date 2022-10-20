89f34421cb7b530b53e7aabdf5d8a5477e9fe329
Saif Kurdi-Teylouni <saif@setsail.co>
2022-10-20 05:15:34 -0400

    completed question 5 third part

--- question5.py
+++ question5.py
@@ -7,23 +7,18 @@ import math
 import numpy as np
 import seaborn as sns
 from question4 import *
+import matplotlib.colors as mcolors
 
 #The function create_full_df can now create a DF with all files in a directory
 
 
-def plot_shots_as_stacked_bars(shots: pd.Series, goals: pd.Series)->pd.DataFrame:
+def plot_shots_as_bubble_chart(shots: pd.Series, goals: pd.Series)->pd.DataFrame:
     """
     Function that takes a directory of json game files, iterates through all of them to create a pd.Dataframe
     where each event of every match of every season in te directory represent one row.
     Only the events of type "Shot" and "Goal" are added to the dataframe
     """
 
-
-    shots = shots.sort_values(ascending=True)
-    goals = goals.reindex(shots.index.values)
-    goals = goals.divide(shots)*100
-    print(goals.index.values)
-    print(goals.tolist())
     # Make some labels.
     labels = goals.tolist()
     labels = [round(l, 1) for l in labels]
@@ -59,42 +54,64 @@ def plot_shots_as_stacked_bars(shots: pd.Series, goals: pd.Series)->pd.DataFrame
     plt.ylabel("Number of shots")
     plt.show()
 
+def plot_shot_placements_as_bubble_chart(shots: pd.Series, goals: pd.Series)->pd.DataFrame:
 
-def aggregate_over_shot_types(df: pd.DataFrame)->(pd.Series, pd.Series):
-    shots = df.groupby(['Shot_Type']).size()
-    goals = df[df['Shot_or_Goal']=='Goal'].groupby(['Shot_Type']).size().groupby(level=0).max()
+    # Make some labels.
+    labels = goals.tolist()
+    labels = [round(l, 1) for l in labels]
+    sns.set(rc = {'figure.figsize':(15,8)})
+    goals = goals.reset_index()
+    goals.rename({0:'goal_percent'},axis='columns',inplace=True)
+
+    print(goals.columns.values)
+    print(goals)
+    data = pd.DataFrame({'range':goals.range, 'goals %':goals.goal_percent, 'Shot type': goals.Shot_Type})
+    # use the scatterplot function
+    sns.scatterplot(data=data, x="range", y="goals %", hue="Shot type", cmap="viridis", edgecolors="black", alpha=0.5, sizes=(10, 1000))
+        # Add titles (main and on axis)
+    plt.ylabel("Goal %")
+    plt.show()
+
+def aggregate_over_shot_types(df: pd.DataFrame, include_range=False)->(pd.Series, pd.Series):
+    groups=['Shot_Type']
+    level = [0]
+    if include_range:
+         groups.append('range')
+         level = [0, 1]
+    shots = df.groupby(groups).size()
+    goals = df[df['Shot_or_Goal']=='Goal'].groupby(groups).size().groupby(level=level).max()
     no_goal_shots = shots[~shots.index.isin(goals.index)]
     no_goal_shots.iloc[:] = 0
     goals = pd.concat([goals, no_goal_shots])
-    print('hello')
-    print(no_goal_shots)
-    print(shots)
-    print(goals)
+    shots = shots.sort_values(ascending=True)
+    goals = goals.reindex(shots.index.values)
+    goals = goals.divide(shots)*100
     return (shots, goals)
 
 def split_DF_by_Distances(df: pd.DataFrame):
     metaDF = []
-    for i in range(40):
-        df1 = df[df['Distance']>= i*5]
-        df1 = df1[df1['Distance']< (i*5)+5]
-        metaDF.append(df1)
-    return metaDF
+    df['range'] = df['Distance'] // 5
+    df = df[~df['range'].isna()]
+    df['range'] = df['range'].astype(int)
+    return df
 
-def get_Chances_Goal(list):
+def get_Chances_Goal(df: pd.DataFrame):
     chances = []
-    try:
-        for (i,ex) in enumerate(list):
-            count = 0
-            for event in ex.iterrows():
-                if event[1][5]=="Goal":
-                    count += 1
-            chances.append(count/len(ex))
-    except Exception as e:
-        chances.append(0.0)
+    for i in range(40):
+        count = 0
+        for event in df.iterrows():
+            if event[1][5]=="Goal" and event[1][15] == i:
+                count += 1
+        print(df[df['range']==i].shape[0])
+        try:
+            chances.append(count/df[df['range']==i].shape[0])
+        except Exception as e:
+            chances.append(0.0)
     return(chances)
 
 if __name__ == "__main__":
     directory = r'data_saved'
     df = create_full_df(directory=directory)
     splitDF = split_DF_by_Distances(df)
-    get_Chances_Goal(splitDF)
+    #get_Chances_Goal(splitDF)
+    splitDF = aggregate_over_shot_types(splitDF)
